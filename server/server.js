8b8c29d04c1f4724eae2c55d949e8c7c3ceda07c
noam tamari <noamtamari@gmail.com>
2023-11-17 01:35:28 +0200

    server sending files attempt

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(__dirname + "/public"));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/static')));
 
 
 app.use(cookieParser());
@@ -622,9 +622,8 @@ app.post("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
     // res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 })
-app.get('*', (req, res) => {
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 app.listen(PORT, () => { console.log("server started on port 5000") })
